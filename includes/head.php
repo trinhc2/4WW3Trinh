a6597faba733cb9722f3751684e824769cfa2bfa
Christian Trinh <trinhc2@mcmaster.ca>
2021-11-24 16:43:04 -0500

    Added template files for footer, header and head properties

---
+++ includes/head.php
@@ -0,0 +1,5 @@
+<link rel="stylesheet" href="./styles/global.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto" type="text/css">
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta charset="UTF-8">
